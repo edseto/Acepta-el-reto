f46ce6efaeb744b551531774408f7c0e7284ee69
edseto <edseguito@gmail.com>
2020-02-21 20:25:23 +0100

    Update and rename Festa.java to FiestaAburrida.java

--- FiestaAburrida.java
+++ FiestaAburrida.java
@@ -1,13 +1,16 @@
+//117 - La fiesta aburrida
 import java.util.Scanner;
-public class Festa{
+public class FiestaAburrida{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numPersones, i, posicio=0;
         String presentacio, nom;
         String[] noms;
+        
         numPersones = sc.nextInt();
         presentacio = sc.nextLine();
         noms = new String[numPersones];
+        
         for(i=0;i<numPersones*2;i++){
             if(i<numPersones){
                 presentacio = sc.nextLine();
